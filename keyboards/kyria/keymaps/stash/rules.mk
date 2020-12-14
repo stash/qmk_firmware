10bd9a0b0ceecf13a23b22242ca2ea01dbb2558a
Jeremy Stashewsky <jstash@gmail.com>
2020-12-13 19:50:00 -0800

    prototype keymap and feature config

--- keyboards/kyria/keymaps/stash/rules.mk
+++ keyboards/kyria/keymaps/stash/rules.mk
@@ -1,3 +1,12 @@
+#CONSOLE_ENABLE = yes
+#DEBUG_ENABLE = yes
+
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+LEADER_ENABLE = yes
+DEBOUNCE_TYPE = eager_pk
+COMMON_VPATH += "../source/repos/qmk-private"
+COMMON_VPATH += "../src/qmk-private"
