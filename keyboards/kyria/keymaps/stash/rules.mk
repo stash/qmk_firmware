edef9968a4d590adec413f65e9379e60ed8cbc4e
Jeremy Stashewsky <jstash@gmail.com>
2020-12-18 17:18:43 -0800

    Checkpoint 2020-12-17

--- keyboards/kyria/keymaps/stash/rules.mk
+++ keyboards/kyria/keymaps/stash/rules.mk
@@ -1,12 +1,19 @@
 #CONSOLE_ENABLE = yes
 #DEBUG_ENABLE = yes
+BOOTMAGIC_ENABLE = no
 
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
 LEADER_ENABLE = yes
+DYNAMIC_MARCO_ENABLE = no
+NKRO_ENABLE = yes
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+
 DEBOUNCE_TYPE = eager_pk
 COMMON_VPATH += "../source/repos/qmk-private"
 COMMON_VPATH += "../src/qmk-private"
