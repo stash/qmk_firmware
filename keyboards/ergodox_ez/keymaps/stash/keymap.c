2107db45ba77852204977169dbb393c33d45cbea
Jeremy Stashewsky <jstash@gmail.com>
2022-03-27 13:45:53 -0700

    Make top-right Meh+M, clean up higher layers

--- keyboards/ergodox_ez/keymaps/stash/keymap.c
+++ keyboards/ergodox_ez/keymaps/stash/keymap.c
@@ -41,7 +41,7 @@ enum my_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ergodox_pretty(
-    KC_CVVV,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(_QWERTY),                                    TT(_FNUM),      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MPLY,
+    KC_CVVV,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(_QWERTY),                                    TT(_FNUM),      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           MEH(KC_M),
     KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_LPRN,                                        KC_RPRN,        KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCOLON,      KC_BSLASH,
     KC_EQUAL,       KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                                                           KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_ENTER,                                       KC_LEAD,        KC_K,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_SMIN,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F9,                                          KC_F10,         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     _______,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_F12,                                         KC_F11,         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    KC_LCTRL,       KC_LALT,        KC_LBRACKET,    KC_RBRACKET,    KC_GRAVE,                                                                                                       _______,        _______,        _______,        _______,        KC_RCTRL,
+    KC_LCTRL,       KC_LALT,        _______,        _______,        KC_GRAVE,                                                                                                       _______,        _______,        _______,        _______,        KC_RCTRL,
                                                                                                     KC_DELETE,      KC_INSERT,      KC_HOME,        KC_END,
                                                                                                                     KC_LALT,        KC_PGUP,
                                                                                     KC_SPACE,       KC_ENTER,       KC_LCTRL,       KC_PGDN,        KC_KP_ENTER,    KC_KP_0
@@ -64,21 +64,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CVVV,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          _______,                                        _______,        KC_F6,         KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_MPLY,
     _______,        KC_F11,         KC_F12,         KC_PSCREEN,     KC_NUMLOCK,     KC_PAUSE,       KC_LPRN,                                        KC_RPRN,        KC_VOLU,       KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_SLASH,    KC_BSLASH,
     _______,        KC_MPRV,        KC_MPLY,        KC_MSTP,        KC_MNXT,        KC_SCROLLLOCK,                                                                  KC_MUTE,       KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_ASTERISK, KC_QUOTE,
-    KC_LSHIFT,      LCTL(KC_Z),     LCTL(KC_X),     LCTL(KC_C),     LCTL(KC_D),     LCTL(KC_V),     KC_ENTER,                                       KC_LEAD,        KC_VOLD,       KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_MINUS,    KC_SMIN,
-    KC_SuAT,        ST_MACRO_Sleep, ST_MACRO_Sleep, _______,        KC_GRAVE,                                                                                                      KC_KP_0,        KC_KP_DOT,      KC_KP_DOT,      KC_KP_PLUS,     TT(_ADJUST),
+    KC_LSHIFT,      KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_ENTER,                                       KC_LEAD,        KC_VOLD,       KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_MINUS,    KC_SMIN,
+    KC_SuAT,        ST_MACRO_Sleep, ST_MACRO_Sleep, KC_NO,          KC_GRAVE,                                                                                                      KC_KP_0,        KC_KP_DOT,      KC_KP_DOT,      KC_KP_PLUS,     TT(_ADJUST),
                                                                                                     KC_DELETE,      LGUI_T(KC_TAB), _______,        _______,
                                                                                                                     KC_LALT,        _______,
                                                                                     KC_SPACE,       KC_BSPACE,      KC_LCTRL,       _______,        KC_KP_ENTER,    KC_KP_0
   ),
   [_ADJUST] = LAYOUT_ergodox_pretty(
-    NK_ON,          NK_OFF,         _______,        _______,        _______,        WKSP_DEL,       KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          EEP_RST,        RESET,
+    NK_ON,          NK_OFF,         KC_NO,          KC_NO,          KC_NO,          WKSP_DEL,       KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          EEP_RST,        RESET,
     KC_NO,          KC_MS_BTN4,     KC_MS_UP,       KC_MS_BTN5,     KC_MS_WH_UP,    WKSP_NEXT,      KC_ASTG,                                        KC_ASUP,        KC_NO,          KC_NO,          RGB_TOG,        LED_LEVEL,  TOGGLE_LAYER_COLOR, RGB_SLD,
     KC_MS_ACCEL0,   KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_DOWN,  WKSP_PREV,                                                                      RGB_HUI,        RGB_SAI,        RGB_VAI,        RGB_SPI,        RGB_MOD,        KC_NO,
     KC_LSHIFT,      KC_MS_WH_LEFT,  KC_NO,          KC_MS_WH_RIGHT, KC_NO,          WKSP_NEW,       KC_ASRP,                                        KC_ASDN,        RGB_HUD,        RGB_SAD,        RGB_VAD,        RGB_SPD,        LED_LEVEL,      KC_RSHIFT,
-    _______,        KC_NO,          KC_NO,          _______,        _______,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______,
+    _______,        KC_NO,          KC_NO,          KC_NO,          KC_NO,                                                                                                          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______,
                                                                                                     KC_MS_BTN3,     CG_SWAP,        _______,        _______,
-                                                                                                                    _______,        _______,
-                                                                                    KC_MS_BTN1,     KC_MS_BTN2,     CG_NORM,        _______,        _______,        KC_CAPS
+                                                                                                                    _______,        KC_RALT,
+                                                                                    KC_MS_BTN1,     KC_MS_BTN2,     CG_NORM,        KC_RCTL,        _______,        KC_CAPS
   ),
 
 };
