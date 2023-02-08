529c6fdb40c91606839899d106c72e0ba5a022b9
Jeremy Stashewsky <jstash@gmail.com>
2023-02-08 01:07:03 -0800

    Add layer color, enter on LH

--- keyboards/bastardkb/scylla/keymaps/stash/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/stash/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_split_4x6_5(
   _______, _______, _______, _______, _______, _______,     _______, _______, KC_NUM,  KC_PSLS, KC_PAST, _______,
-  _______, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,     XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+  _______, XXXXXXX, XXXXXXX, KC_UP,   KC_ENT,  XXXXXXX,     XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
   _______, KC_GRV,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,      XXXXXXX, KC_P6,   KC_P5,   KC_P4,   KC_PPLS, _______,
   _______, KC_GRV,  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,     XXXXXXX, KC_P3,   KC_P2,   KC_P1,   KC_PENT, _______,
                              _______, _______, KC_DEL,      KC_PENT, KC_P0,   KC_PDOT,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_split_4x6_5(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,     KC_NUM,  KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, _______,
+  _______, XXXXXXX, XXXXXXX, KC_UP,   KC_ENT,  XXXXXXX,     KC_NUM,  KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, _______,
   _______, KC_GRV,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,      KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
   _______, KC_GRV,  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,     KC_PGDN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
                              KC_LALT, KC_LCTL, KC_DEL,      KC_INS,  KC_RALT, KC_MENU,
@@ -74,8 +74,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-};
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+    switch(get_highest_layer(state)) {
+        case _LOWER:
+            rgb_matrix_sethsv_noeeprom(HSV_GOLD);
+            break;
+        case _RAISE:
+            rgb_matrix_sethsv_noeeprom(HSV_BLUE);
+            break;
+        case _ADJUST:
+            rgb_matrix_sethsv_noeeprom(HSV_RED);
+            break;
+        default:
+            rgb_matrix_sethsv_noeeprom(HSV_CYAN);
+            break;
+    }
+    return state;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
