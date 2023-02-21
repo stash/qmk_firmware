1f5517d8cd28940eade8fde385f15a92f8438015
Jeremy Stashewsky <jstash@gmail.com>
2023-02-20 18:43:11 -0800

    More layout and tapdance fun

--- keyboards/bastardkb/scylla/keymaps/stash/rules.mk
+++ keyboards/bastardkb/scylla/keymaps/stash/rules.mk
@@ -1,1 +1,6 @@
 TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+DEBOUNCE_TYPE = sym_defer_pk
+# 45 seemed to work pretty good, keep uping by 5-10 until it consistently stops happening
+DEBOUNCE = 50
