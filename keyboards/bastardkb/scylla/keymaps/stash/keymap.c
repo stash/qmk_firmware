f8491600c62677f41ffada717ae4271ac223a847
Jeremy Stashewsky <jstash@gmail.com>
2024-09-02 17:12:28 -0700

    QWERTY game layer, remove mousekeys

--- keyboards/bastardkb/scylla/keymaps/stash/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/stash/keymap.c
@@ -23,9 +23,9 @@
 
 extern keymap_config_t keymap_config;
 
-enum layer_names { _WINDOWS, _MAC, _R_HAND, _L_HAND, _BOTH_HANDS };
+enum layer_names { _WINDOWS, _MAC, _GAME, _R_HAND, _L_HAND, _BOTH_HANDS };
 
-enum custom_keycodes { SET_WIN = SAFE_RANGE, SET_MAC, RGB_RLD };
+enum custom_keycodes { SET_WIN = SAFE_RANGE, SET_MAC, RGB_RLD, SET_GAM };
 
 enum dance_codes { DANCE_CVVV = 0, DANCE_MAX };
 #define TD_CVVV TD(DANCE_CVVV)
@@ -57,13 +57,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_LCTL, TT(_L_HAND),     TT(_R_HAND), KC_RGUI
   ),
 
+[_GAME] = LAYOUT_split_4x6_5(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_GRV,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLS,
+  KC_EQUAL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,        KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,        KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+                             KC_LALT, KC_SPC,  KC_BSPC,     KC_ENT, KC_RSFT, OSM(MOD_MEH),
+                                  KC_LCTL, TT(_L_HAND),     TT(_R_HAND), KC_RGUI
+  ),
+
+
 [_R_HAND] = LAYOUT_split_4x6_5(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_MINS, KC_SCLN, KC_BSLS, KC_LCBR, KC_RCBR, MAC_SSC,     KC_WH_U, KC_BTN4, XXXXXXX, XXXXXXX, KC_BTN5, KC_ACL2,
-  KC_QUOT, KC_COMM, KC_DOT,  KC_LPRN, KC_RPRN, WIN_SSC,     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL1,
-  _______, KC_GRV,  KC_TILD, KC_LBRC, KC_RBRC, KC_ENT,      XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ACL0,
-                             _______, _______, KC_DEL,      KC_BTN3, KC_BTN1, KC_BTN2,
-                                      _______, _______,     _______, _______
+  KC_MINS, KC_SCLN, KC_BSLS, KC_LCBR, KC_RCBR, MAC_SSC,     XXXXXXX, A(KC_LEFT), A(KC_DOWN), A(KC_UP), A(KC_RGHT), _______,
+  KC_QUOT, KC_COMM, KC_DOT,  KC_LPRN, KC_RPRN, WIN_SSC,     XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+  _______, KC_GRV,  KC_TILD, KC_LBRC, KC_RBRC, KC_ENT,      XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+                             KC_LALT, KC_LCTL, KC_DEL,      KC_INS,  CW_TOGG, _______,
+                                      KC_LGUI, _______,     _______, KC_MENU
   ),
 
 [_L_HAND] = LAYOUT_split_4x6_5(
@@ -71,12 +81,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MINS, KC_SCLN, KC_BSLS, KC_LCBR, KC_RCBR, MAC_SSC,     XXXXXXX, A(KC_LEFT), A(KC_DOWN), A(KC_UP), A(KC_RGHT), _______,
   KC_QUOT, KC_COMM, KC_DOT,  KC_LPRN, KC_RPRN, WIN_SSC,     XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
   _______, KC_GRV,  KC_TILD, KC_LBRC, KC_RBRC, KC_ENT,      XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
-                             KC_LALT, KC_LCTL, KC_DEL,      KC_INS,  CW_TOGG, KC_MENU,
-                                      KC_LGUI, _______,     _______, _______
+                             KC_LALT, KC_LCTL, KC_DEL,      KC_INS,  CW_TOGG, _______,
+                                      KC_LGUI, _______,     _______, KC_MENU
   ),
 
 [_BOTH_HANDS] = LAYOUT_split_4x6_5(
-  QK_BOOT, QK_REBOOT,EE_CLR, XXXXXXX, XXXXXXX, SET_WIN,     SET_MAC, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_NUM,
+  QK_BOOT, QK_REBOOT,EE_CLR, XXXXXXX, SET_GAM, SET_WIN,     SET_MAC, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_NUM,
   RGB_RLD, RGB_SPI, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,     KC_TAB,  KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_PSCR,
   RGB_TOG, RGB_SPD, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,    KC_EQUAL,KC_P6,   KC_P5,   KC_P4,   KC_PMNS, KC_SCRL,
   KC_LSFT, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_G, KC_RSFT,     KC_LPRN, KC_P3,   KC_P2,   KC_P1,   KC_PPLS, KC_PAUS,
@@ -113,8 +123,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SET_WIN:
+        case SET_GAM:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_WINDOWS);
+                set_single_persistent_default_layer(keycode == SET_WIN ? _WINDOWS : _GAME);
                 keymap_config.raw = eeconfig_read_keymap();
                 keymap_config.swap_lctl_lgui = keymap_config.swap_rctl_rgui = 0;
                 eeconfig_update_keymap(keymap_config.raw);
--- keyboards/bastardkb/scylla/keymaps/stash/rules.mk
+++ keyboards/bastardkb/scylla/keymaps/stash/rules.mk
@@ -1,6 +1,5 @@
 TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
-MOUSEKEY_ENABLE = yes
 
 DEBOUNCE_TYPE = sym_defer_pk
 # 45 seemed to work pretty good, keep uping by 5-10 until it consistently stops happening
